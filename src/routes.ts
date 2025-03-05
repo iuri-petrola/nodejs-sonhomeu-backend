5964be0c8f4294e9cf337f2b65922ac2553c4208
Iuri Petrola <iuri.petrola@gmail.com>
2025-03-04 23:26:06 -0300

    Alterando path da imagem do produto

--- src/routes.ts
+++ src/routes.ts
@@ -29,7 +29,7 @@ import uploadConfig from './config/multer'
 
 const router = Router();
 
-const upload = multer(uploadConfig.upload("./tmp"));
+const upload = multer(uploadConfig.upload("./img"));
 
 //-- ROTAS USER --
 router.post('/users', new CreateUserController().handle)
