883294d337316a6faa7a2912349c1d2556f76df5
Iuri Petrola <iuri.petrola@gmail.com>
2025-06-03 22:50:47 -0300

    adiçao da variavel JWT_SECRET

--- docker-run.sh
+++ docker-run.sh
@@ -14,7 +14,7 @@
    docker run -itd --name postgresql16 -h postgresql16  --net net_someu --restart unless-stopped  -p 5432:5432  -v postgresql16:/etc/postgresql/16/main iuripetrola/postgresql-someu:16
 
 # Iniciar container App
-   docker run -itd --name nodejs-someu-back -h nodejs-someu-back --net net_someu --restart unless-stopped  -p 8080:8080 --env DATABASE_URL="postgresql://user_nodejs_someu:pass_nodejs_someu@postgresql16:5432/db_nodejs_someu?schema=public" --mount type=bind,source=/mnt/img/,target=/mnt/img/ iuripetrola/nodejs-someu-back:latest
+   docker run -itd --name nodejs-someu-back -h nodejs-someu-back --net net_someu --restart unless-stopped  -p 8080:8080 --env DATABASE_URL="postgresql://user_nodejs_someu:pass_nodejs_someu@postgresql16:5432/db_nodejs_someu?schema=public" --env JWT_SECRET=fbc878419638a610508320c876597c83 --mount type=bind,source=/mnt/img/,target=/mnt/img/ iuripetrola/nodejs-someu-back:latest
     
    #--mount type=bind,source=/home/iuri/iuri.petrola@gmail.com-OneDrive/Sistema-Sonho-Meu-Kids/Nodejs-Sonho-meu-kids/,target=/app
 
